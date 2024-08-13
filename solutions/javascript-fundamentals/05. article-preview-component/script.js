dd42357b966f2deb7f5c5e7dea3cc6c21cc8dc49
Meryem EL KARATI <elkaratimeryem@gmail.com>
2024-08-13 17:59:49 +0200

    Add the share bubble for large screens

--- solutions/javascript-fundamentals/05. article-preview-component/script.js
+++ solutions/javascript-fundamentals/05. article-preview-component/script.js
@@ -4,6 +4,5 @@ const share_btns = document.querySelectorAll('.share-btn');
 share_btns.forEach((share_btn) => {
     share_btn.addEventListener('click', function() {
             sharebar.classList.toggle('visible');
-
     });
 });

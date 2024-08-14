faab670f7bf2acbc23ffa59145f80ea796bf02ef
Meryem EL KARATI <elkaratimeryem@gmail.com>
2024-08-14 15:31:27 +0200

    Implement Javascript logic

--- solutions/javascript-fundamentals/06. newsletter-sign-up/script.js
+++ solutions/javascript-fundamentals/06. newsletter-sign-up/script.js
@@ -1,10 +1,22 @@
-const form = document.getElementById('signup-form');
+const forms = document.querySelectorAll('form');
 const signup = document.getElementById('signup');
 const signup_success = document.getElementById('signup-success');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+for(const form of forms) {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-    signup.style.display = "none";
-    signup_success.style.display = "flex";
-})
+        const email = document.getElementById("emailInput").value;
+        document.getElementById("success-email").textContent = email;
+
+        if (signup.style.display === "none" || signup.style.display === "") {
+            // When signup is hidden, show it and hide signup_success
+            signup.style.display = "flex";
+            signup_success.style.display = "none";
+        } else {
+            // When signup is visible, hide it and show signup_success
+            signup.style.display = "none";
+            signup_success.style.display = "flex";
+        }
+    });
+}

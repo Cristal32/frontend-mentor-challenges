49fae5e8be99cc2cca03901992ae0b49c87f04c4
Meryem EL KARATI <elkaratimeryem@gmail.com>
2024-08-17 22:59:09 +0200

    Add error handling for newsletter signup challenge

--- solutions/javascript-fundamentals/06. newsletter-sign-up/script.js
+++ solutions/javascript-fundamentals/06. newsletter-sign-up/script.js
@@ -1,22 +1,33 @@
 const forms = document.querySelectorAll('form');
 const signup = document.getElementById('signup');
 const signup_success = document.getElementById('signup-success');
+const emailInput = document.getElementById("emailInput");
+const emailError = document.getElementById("email-error");
 
 for(const form of forms) {
     form.addEventListener('submit', (e) => {
         e.preventDefault();
 
-        const email = document.getElementById("emailInput").value;
-        document.getElementById("success-email").textContent = email;
+        // Check if email input is valid
+        emailError.textContent = "";
+        const emailValue = emailInput.value.trim();
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-        if (signup.style.display === "none" || signup.style.display === "") {
-            // When signup is hidden, show it and hide signup_success
-            signup.style.display = "flex";
-            signup_success.style.display = "none";
+        if (emailValue === "" || !emailPattern.test(emailValue)) {
+            emailError.style.display = "inline-block";
+            emailInput.focus();
         } else {
-            // When signup is visible, hide it and show signup_success
-            signup.style.display = "none";
-            signup_success.style.display = "flex";
+            document.getElementById("success-email").textContent = emailValue;
+
+            if (signup.style.display === "none" || signup.style.display === "") {
+                // When signup is hidden, show it and hide signup_success
+                signup.style.display = "flex";
+                signup_success.style.display = "none";
+            } else {
+                // When signup is visible, hide it and show signup_success
+                signup.style.display = "none";
+                signup_success.style.display = "flex";
+            }
         }
     });
 }
